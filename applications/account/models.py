1b2d6f52ffcce475b6c6126de5df12437b5f4613
aanastasiyatuz <aanastasiyatuz@gmail.com>
2021-11-28 21:54:23 +0600

    migrations

--- applications/account/models.py
+++ applications/account/models.py
@@ -27,6 +27,7 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractUser):
+    username = None
     email = models.EmailField(unique=True)
     is_active = models.BooleanField(default=False)
     activation_code = models.CharField(max_length=20, blank=True)
